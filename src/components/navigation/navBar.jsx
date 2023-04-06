d1148c4e0dfd6bb026e1fc430463ceed34876027
Danny Chung <chungdanny56@gmail.com>
2023-04-06 10:49:50 -0700

    Started implementation of routing, added links to navbar, created errorPage component TODO: Check and adjust navbar on mobile sizes

--- src/components/navigation/navBar.jsx
+++ src/components/navigation/navBar.jsx
@@ -5,8 +5,11 @@ import './navBar.css';
 const NavBar = () => {
     return (
     <div className = "navbar-container">
-        <h1 className="website-name">Trash & Treasure</h1>
-        
+        <h1 className="website-name"><a className="title-link" href={`/`}>Trash & Treasure</a></h1>
+        <nav className="link-container">
+            <a className="link" href={`/aboutPage`}>About</a>
+            <a className="link" href={`/loginPage`}>Login</a>
+        </nav>
     </div>
     )
 }
---
+++ src/pages/errorPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+FrontPage = () => {
+    return(<h1>FrontPage</h1>);
+}
+
+export default FrontPage;
