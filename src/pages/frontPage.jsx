69012caf863f0eed6f14685b22b96c9ffefdac25
Danny Chung <chungdanny56@gmail.com>
2023-04-06 11:40:00 -0700

    Basic routing set up, going to start working on login form, general page css, and displaying data from JSON

--- src/pages/frontPage.jsx
+++ src/pages/frontPage.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from 'react';
 
+
+/*
+    This will be responsible for loading data from the database into cards for display
+*/
 const FrontPage = () => {
     return(<h1>FrontPage</h1>);
 }
