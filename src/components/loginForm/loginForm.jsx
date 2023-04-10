cb6e1139abdf1faa83253853dbadd78495031990
Danny Chung <chungdanny56@gmail.com>
2023-04-10 14:16:34 -0700

    Attempting to add authentication to front end

--- src/components/loginForm/loginForm.jsx
+++ src/components/loginForm/loginForm.jsx
@@ -5,13 +5,13 @@ import './loginForm.css';
 const LoginForm = () => {
     return(
         <form className = "login-form">
-            <fieldset>
+            <fieldset className="login-set">
                 <legend>Login</legend>
                 <label htmlFor="username">Username: </label>
-                <input type="text" name="username"></input>
+                <input type="text" name="username" required></input>
                 <label htmlFor = "password">Password: </label>
-                <input type="password" name="password"></input>
-                <input type="submit" value="Login"></input>
+                <input type="password" name="password" required></input>
+                <input className="login-button" type="submit" value="Login"></input>
             </fieldset>
         </form>
     );
--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import './pagestyles.css'
 
 const AboutPage = () => {
-    return(<h1>AboutPage</h1>);
+    return(
+        <section className="about-page main-content">
+            <h2 className="title">Welcome to Trash and Treasure!</h2>
+            <p className="about-content">
+                "Someone's trash is another person's treasure". This is a sentiment that I want to demonstrate with this website.
+                Post your personal belongings and let other people rate them! You can also rate other people's
+                belongings. Everything is subjective, and not everyone will have the same opinion as you. You must make an
+                account to rate or post.
+            </p>
+        </section>
+    );
 
 }
 
