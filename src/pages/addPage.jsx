5cad9f7acf2c1e021b15814fc34e6874959c3aff
Danny Chung <chungdanny56@gmail.com>
2023-04-12 11:37:42 -0700

    Changed title in index.html, created addPage.jsx

---
+++ src/pages/addPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+import './pagestyles.css';
+
+const AddPage = () => {
+    return(
+        <div className="main-content">
+        </div>
+    );
+}
+
+export default AddPage;
